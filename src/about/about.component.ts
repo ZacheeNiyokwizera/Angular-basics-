f24d89c83f25d1eb795c962e3a9379de1beb7210
Zachee <zack@labs.ws>
2023-06-16 18:37:03 +0100

    added some new features

--- src/about/about.component.ts
+++ src/about/about.component.ts
@@ -12,12 +12,21 @@ export class AboutComponent {
     age : number = 27;
     message: string = 'We are one text away';
     kintiaWillText = false;
+    showSecret = false;
+    log: number[] = []
     constructor(){
      
     }
     onSendMessage(){
       this.kintiaWillText = true;
       //  this.message = 'Thanks for contacting KK';
+    }
+
+    toggleDisplay(){
+      this.showSecret = !this.showSecret;
+      this.log.push(this.log.length + 1);
+      // this.log.push(new Date());
+
     }
      
 
